14390c90afc3cd61389d2e96749865c10042b858
aecordova <ae_cordova@outlook.com>
2020-08-27 12:43:46 -0500

    Add i18n and fix checkout page

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,9 @@ module SolidusStroe
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.i18n.available_locales = [:es, :en]
+    config.i18n.default_locale = :es
+    config.i18n.fallbacks = true
   end
 end
